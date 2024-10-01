241be2ca0ea8c1742e92b271ca561856cf45b3a0
Szabo-Gergo <s.gergo1432@gmail.com>
2024-10-01 21:44:09 +0100

    Polsihing animation, making charge attack.
    Aligning sword trail to sword swipe (still needs work), semi complete charge attack needs polish but in general pretty much done.

--- Scripts/attack.gd
+++ Scripts/attack.gd
@@ -5,6 +5,7 @@ class_name Attack
 @export var slide_speed : float = 0
 @onready var animation_tree = %AnimationTree
 @onready var character_body_2d: CharacterBody2D = $"../.."
+@onready var animation_player: AnimationPlayer = $"../../AnimationPlayer"
 
 var mouse_position : Vector2
 var charged_attack : bool
@@ -13,39 +14,36 @@ var charge_time : float
 
 
 func enter(inputs : Dictionary = {}):
-	charge_time = 5
+	charge_time = 0
 	charged_attack = false
 	is_charging = false
 	mouse_position = inputs.get("mouse_position")
 	animation_tree.set("parameters/Attack1/blend_position", mouse_position)
 	animation_tree.set("parameters/Attack2/blend_position", mouse_position)
-
-
+	animation_tree.set("parameters/ChargeAttack/blend_position", mouse_position)
+	animation_tree.set("parameters/ChargeUp/blend_position", mouse_position)
 
 func physics_process(delta: float) -> void:
 		character_body_2d.velocity = mouse_position*slide_speed
 		character_body_2d.move_and_slide()
 		
-#		attack click-re vált state-et ha azt a gombot elengedjük ha nincs
 		if Input.is_action_just_released("attack"):
+			animation_tree.set("parameters/conditions/charge_up", false)
 			if charged_attack:
-#			play charged attack animation
+				animation_tree.set("parameters/conditions/charged_attack", true)
 				pass
 			else:
 				animation_tree.set("parameters/conditions/attacking", true)
-		
-
-			
+				
 		if Input.is_action_just_pressed("attack"):
-			animation_tree.set("parameters/conditions/combo", true)
-		
-		if Input.is_action_pressed("attack"):
-			charge_time -= delta
-			if charge_time <= 4.9:
-#				PLAY CHARGE ANIMATION
+			animation_tree.set("parameters/conditions/combo", true)	
+		elif Input.is_action_pressed("attack"):
+			charge_time += delta
+			if charge_time >= 0.5:
+				animation_tree.set("parameters/conditions/charge_up", true)
 				is_charging = true
 						
-		if charge_time <= 0:
+		if charge_time >= 4:
 			charged_attack = true
 		
 		if Input.is_action_just_pressed("dash") and Global.available_dash >= 1 and character_body_2d.velocity != Vector2.ZERO:
@@ -55,12 +53,14 @@ func physics_process(delta: float) -> void:
 			
 func _on_attack_finished(anim_name: StringName) -> void:	
 	if "Attack" in anim_name and !animation_tree.get("parameters/conditions/combo"):
-		animation_tree.set("parameters/conditions/combo", false)
 		animation_tree.set("parameters/conditions/attacking", false)
-
 		transition.emit(self, "move") 
+		
 	elif "Attack2" in anim_name:
 		animation_tree.set("parameters/conditions/combo", false)
 		animation_tree.set("parameters/conditions/attacking", false)
-
 		transition.emit(self, "move") 
+
+	elif  "ChargeAttack" in anim_name:
+		animation_tree.set("parameters/conditions/charged_attack", false)
+		animation_tree.set("parameters/conditions/attacking", false)
